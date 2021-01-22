1a5350b566ffc21786c6f300308b9b7cea2446b0
XueMoMo <2681350846@qq.com>
2021-01-22 16:18:20 +0000

    Automated release: 2021-01-22, 16:18:20

---
+++ docs/_next/static/hgT5d_zK3S78CaFVR8xTa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-b4671be15bf1c8f148a2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-855fb8f51be1a11b3953.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
